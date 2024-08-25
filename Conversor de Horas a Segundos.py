29ea655a91b5fd5680d83d99da0434c24b25f0a0
Martin Molina <74263137+martinmol2007@users.noreply.github.com>
2024-08-25 23:40:04 +0200

    Add files via upload

--- Conversor de Horas a Segundos.py
+++ Conversor de Horas a Segundos.py
@@ -1,6 +1,6 @@
-print ("Bienvenido a mi conversor de horas a segundos!")
+print ("¡Bienvenido a mi conversor de horas a segundos!")
 
-horas = float(input("Inroduce el numero de horas que deseas convertir a segundos:"))
+horas = float(input("Introduce el número de horas que deseas convertir a segundos:"))
 segundos = horas * 3600
 
 if horas <= 1:
